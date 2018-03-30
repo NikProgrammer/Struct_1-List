53939274579faace0aadc8ae83d45235bd70ece3
NikProgrammer <NikNovikJ@yandex.ru>
2018-03-30 07:25:23 +0300

    Почти готовая библиотека

--- Source.cpp
+++ Source.cpp
@@ -1,19 +1,32 @@
-#include "List.h"
+#include "myList.h"
 
 
 int main()
 {
+	MyList::List<int> testList = MyList::CreateList(15);
+	//MyList::Insert(testList, 75);
+	MyList::Insert(MyList::Insert(testList, 75), 93);
 
+	MyList::Display(testList);
+
+	//printf_s("testList = %d, next = %d, next = %d\n", testList->Data, testList->Next->Data, testList->Next->Next->Data);
+
+	/*MyList::List<int> test2List = MyList::CreateList(63);
+	MyList::Insert(test2List, 81);
+	printf_s("\ntest2List = %d, next = %d\n", test2List->Data, test2List->Next->Data);
 	
+	MyList::Insert(testList, test2List);
 
-	ITStep::List::List<int> Object = ITStep::List::Create(10);
-	ITStep::List::Insert(Object, 20);
-	ITStep::List::List<int> Object1 = Object->Next;
+	printf_s("\ntestList = %d, next = %d, next = %d, next = %d, next = %d\n", testList->Data, testList->Next->Data, testList->Next->Next->Data
+		, testList->Next->Next->Next->Data, testList->Next->Next->Next->Next->Data);
 
-	printf_s("%d\n", ITStep::List::Insert(Object, 20)->Value);
-	printf_s("%d\n", Object->Next->Previous->Value);
-	printf_s("%d\n", Object1->Value);
-	//printf_s("%d\n", ITStep::List::Add(ITStep::List::GetTail(Object), 80)->Value);
+	printf_s("\n%d", MyList::Lenght(testList));
+
+	MyList::Delete(testList->Next, testList->Next->Next);
+	
+	printf_s("\ntestList = %d, next = %d, next = %d\n", testList->Data, testList->Next->Data, testList->Next->Next->Data);
+
+	printf_s("\n%d", MyList::Lenght(testList));*/
 
 	return 0;
 }
---
+++ myList.h
@@ -0,0 +1,172 @@
+#pragma once
+
+#include <stdlib.h>
+#include <stdio.h>
+namespace MyList {
+	
+	//Определение структуры
+	template <typename T>
+	struct _MyList
+	{
+		T Data;
+
+		struct _MyList *Next;
+		struct _MyList *Previous;
+	};
+	
+	//Определение указателя на структуру
+	template <typename T>
+	using List = struct _MyList<T>*;
+	
+	//Функция создания элемента структуры
+	template <typename T>
+	List<T> CreateList(const T & Value)
+	{
+		List<T> Object = (List<T>)malloc(sizeof(struct _MyList<T>));
+		Object->Data = Value;
+		Object->Next = nullptr;
+		Object->Previous = nullptr;
+
+		return Object;
+	}
+
+	//Проверка на корневой элемент
+	template <typename T>
+	bool IsRoot(const List<T> Object)
+	{
+		return(Object->Previous) ? false : true;
+	}
+
+	//Проверка на последний элемент
+	template <typename T>
+	bool IsTail(const List<T> Object)
+	{
+		return(Object->Next) ? false : true;
+	}
+
+	//Возвращает корневой элемент
+	template <typename T>
+	List<T> GetRoot(List<T> Object)
+	{
+		while (!IsRoot(Object))
+		{
+			Object = Object->Previous;
+		}
+
+		return Object;
+	}
+
+	//Возвращает последний элемент
+	template <typename T>
+	List<T> GetTail(List<T> Object)
+	{
+		while (!IsTail(Object))
+		{
+			Object = Object->Next;
+		}
+
+		return Object;
+	}
+
+	template <typename T>
+	void Destroy(List<T> Object)
+	{
+		if (Object == nullptr)
+			return;
+
+		free(Object);
+	}
+	//вставка цепочки/элемента
+	template <typename T>
+	List<T> Insert(List<T> Object, List<T> NewObject)
+	{
+		List<int> Root = GetRoot(NewObject);
+		List<int> Tail = GetTail(NewObject);
+
+		Root->Previous = Object;
+		
+		if (Object->Next)
+		{
+			Tail->Next = Object->Next;
+			Object->Next->Previous = Tail;
+		}
+		
+		Object->Next = Root;
+
+		return NewObject;
+	}
+	//вставка цепочки/элемента
+	template <typename T>
+	List<T> Insert(List<T> Object, const T & Value)
+	{
+		return Insert(Object, CreateList(Value));
+	}
+	//удаление элемента
+	template <typename T>
+	void Delete(List<T> Object)
+	{
+		Object->Previous->Next = Object->Next;
+		Object->Next->Previous = Object->Previous;
+
+		free(Object);
+
+	}
+	//удаление цепочки
+	template <typename T>
+	void Delete(List<T> RObject, List<T> TObject)
+	{
+		RObject->Previous->Next = TObject->Next;
+		TObject->Next->Previous = RObject->Previous;
+
+		free(RObject);
+		free(TObject);
+
+	}
+
+	//возвращает длину списка
+	template <typename T>
+	int Lenght(List<T> list)
+	{
+		int lenght = 0;
+		if (list == nullptr)
+		{
+			return lenght;
+		}
+
+		List<int> Root = GetRoot(list);
+		lenght++;
+		while (!IsTail(Root))
+		{
+			Root = Root->Next;
+			lenght++;
+		}
+
+		return lenght;
+	}
+
+	template <typename T>
+	int Display(List<T> list)
+	{
+		if (list == nullptr)
+		{
+			return -1;
+		}
+
+		if (Lenght(list) == 1)
+		{
+			printf_s("%d ", list->Data);
+			return 0;
+		}
+
+		while (true)
+		{
+			printf_s("%d ", list->Data);
+			list = list->Next;
+			if (IsTail(list))
+			{
+				printf_s("%d ", list->Data);
+				return 0;
+			}
+		}
+	}
+}
